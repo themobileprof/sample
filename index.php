d41cedb661d17c2cc225a8c864d298e77ac20411
Samuel Anyaele <samuelanyaele@gmail.com>
2020-02-18 13:43:45 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Samuel Anyaele";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>The Sample Website</title>
+</head>
+<body>
+	<h1>Our First  Website</h1>
+	<h2>Welcome <?php echo $name;?> </h2>
+	<p>This is a file to show that you can work on </p>
+</body>
+</html>
